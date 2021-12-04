f6e8ad32cc0d4ae0eef1a165dade93caafdb648d
zzjzzz <zzjzzz.creator@gmail.com>
2021-12-04 16:14:36 -0500

    finished model +logo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,6 +26,30 @@ class ReviewsController < ApplicationController
     def show
         @review = Review.find(params[:id])
     end
+
+    def destroy
+        #delete review from database
+        @review = Review.find(params[:id])
+        @review.destroy
+        redirect_to root_path
+    end
+
+    def edit
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(params.require(:review).permit(:game, :description, :score, :url, :blockchain))
+
+        #save to database
+        if @review.save
+            redirect_to review_path(@review)
+            flash[:message] = "Your Review was Created Successfully!"
+        else
+           flash[:message] = "Your Review was not Created Successfully!"
+        end
+    end
     
 
 end
