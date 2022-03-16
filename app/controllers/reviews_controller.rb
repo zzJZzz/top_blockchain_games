9cb59beb6e36816c35b4f7ce14a074a279a1cc39
zzjzzz <zzjzzz.creator@gmail.com>
2022-03-16 15:43:30 -0400

    Redirects
    If review saved redirect  & notices.
    Validation blockchain presence

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,55 +1,53 @@
 class ReviewsController < ApplicationController
+  def index
+    # list page for reviews
+    @reviews = Review.all
+  end
 
-    def index
-        #list page for reviews
-        @reviews = Review.all
+  def new
+    # form for new review
+    @review = Review.new
+  end
+
+  def create
+    # create new review in database
+    @review = Review.new(params.require(:review).permit(:game, :description, :score, :url, :blockchain))
+
+    # save to database
+    if @review.save
+      redirect_to root_path
+      flash[:message] = "Your Review was Created Successfully!"
+    else
+      flash[:message] = "Your Review was NOT Created Successfully!"
+      render "new"
     end
+  end
 
-    def new
-        #form for new review
-        @review = Review.new
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def destroy
+    # delete review from database
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(params.require(:review).permit(:game, :description, :score, :url, :blockchain))
+
+    # save to database
+    if @review.save
+      redirect_to review_path(@review)
+      flash[:message] = "Your Review was Created Successfully!"
+    else
+      flash[:message] = "Your Review was not Created Successfully!"
     end
-
-    def create
-        #create new review in database
-        @review = Review.new(params.require(:review).permit(:game, :description, :score, :url, :blockchain))
-
-        #save to database
-        if @review.save
-            redirect_to root_path
-            flash[:message] = "Your Review was Created Successfully!"
-        else
-           flash[:message] = "Your Review was not Created Successfully!"
-        end
-    end
-
-    def show
-        @review = Review.find(params[:id])
-    end
-
-    def destroy
-        #delete review from database
-        @review = Review.find(params[:id])
-        @review.destroy
-        redirect_to root_path
-    end
-
-    def edit
-        @review = Review.find(params[:id])
-    end
-
-    def update
-        @review = Review.find(params[:id])
-        @review.update(params.require(:review).permit(:game, :description, :score, :url, :blockchain))
-
-        #save to database
-        if @review.save
-            redirect_to review_path(@review)
-            flash[:message] = "Your Review was Created Successfully!"
-        else
-           flash[:message] = "Your Review was not Created Successfully!"
-        end
-    end
-    
-
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :body, length: {minimum: 20}
   validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
+  validates :blockchain, presence: true
 end
